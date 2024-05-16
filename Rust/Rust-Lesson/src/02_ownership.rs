73a47b4947d2be21d2c073d0d9ff80a3ae117ab5
abe-portfolio <xhskwii@icloud.com>
2024-05-17 01:40:25 +0900

    20240516 Rust-lesson Go-webapplication(3/5)

--- Rust/Rust-Lesson/src/02_ownership.rs
+++ Rust/Rust-Lesson/src/02_ownership.rs
@@ -2,4 +2,15 @@
 
 fn main() {
     // ノートの「letとlet mutとconst」を参照
+    let i:i32 = 1;
+    println!("The value of i is {}", i);
+    println!("The memory address of i is {:p}", &i);
+    {
+        // ブロックを作成してスコープを明示的に記載する
+        let i:i32 = 100;
+        println!("The value of i is {}", i);
+        println!("The memory address of i is {:p}", &i);
+    }
+    println!("The value of i is {}", i);
+    println!("The memory address of i is {:p}", &i);
 }
