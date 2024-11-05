49dd4b95fe51bd82da2162a95a3714bf00dbac15
abe-portfolio <xhskwii@icloud.com>
2024-11-05 23:10:32 +0900

    20241105 Go todo

--- Go/Go_chanel/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/abe-portfolio/Go_chanel
-
-go 1.21.6
---
+++ Go/go-ToDo/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	//"github.com/labstack/echo/v4"
+)
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+/*
+func main() {
+    // Echoインスタンスの作成
+    e := echo.New()
+
+    // ルートエンドポイントの設定
+    e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello, Echo!")
+    })
+
+    // サーバーの開始
+    e.Start(":8080")
+}
+*/
